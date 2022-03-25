7f1a50e0e1a1a7d5dffd1053a0cafec3a18c3d5e
Julian Alfonso Ponce <ponce.julianalfonso@gmail.com>
2022-03-25 19:20:34 +1100

    feat: added functionality to increment total_gifs counter

--- programs/mysolanaproject/src/lib.rs
+++ programs/mysolanaproject/src/lib.rs
@@ -6,10 +6,41 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod mysolanaproject {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result<()> {
+        // Get a reference to the account.
+        let base_account = &mut ctx.accounts.base_account;
+
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+
+    // Another function that addsa a gif
+    pub fn add_gif(ctx: Context<AddGif>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs += 1;
         Ok(())
     }
 }
 
+// Attach certain variables to the StartStuffOff context.
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// Specify what data you want in the AddGif context.
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
--- tests/mysolanaproject.js
+++ tests/mysolanaproject.js
@@ -1,13 +1,56 @@
 const anchor = require("@project-serum/anchor");
 
-describe("mysolanaproject", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+// Need the system program, will talk about this soon.
+const { SystemProgram } = anchor.web3;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Mysolanaproject;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+const main = async () => {
+  console.log("🚀 Starting test...");
+
+  // Create and set the provider. We set it before we needed to update it, so that it can communicate with our frontend
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Mysolanaproject;
+
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Call start_stuff_off, pass it the params it needs!
+  const tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
   });
-});
+
+  console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+
+  // Add gif!
+  await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+
+  // Get the account again to see what changed.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
